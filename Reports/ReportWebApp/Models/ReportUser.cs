309387b5ac0af19ad9cce309eb449783c55a47f6
Philip <philip@spacerush.app>
2019-04-21 10:47:52 -0700

    Use a database called Reports instead of SpacePlanets.
    Also, only create users if no user exists!

--- Reports/ReportWebApp/Models/ReportUser.cs
+++ Reports/ReportWebApp/Models/ReportUser.cs
@@ -8,8 +8,10 @@ namespace ReportWebApp.Models
     public class ReportUser : MongoDbGenericRepository.Models.Document
     {
         public string Rank { get; set; }
-        public string OrganizationRole { get; set; }
         public string Username { get; set; }
         public string PasswordHash { get; set; }
+        public bool IsOrganizationAdmin { get; set; }
+        public List<string> OrganizationRoles { get; set; }
+
     }
 }
